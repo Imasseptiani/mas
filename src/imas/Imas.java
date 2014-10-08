5eace26ce359085ff9f941464ef27bec4b846188
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:33:35 +0700

    commit

---
+++ src/imas/Imas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imas;
+
+/**
+ *
+ * @author User
+ */
+public class Imas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Imas Septiani";
+        String kelas = "SI 13 E";
+        String npm = "13311099";
+        System.out.println("Nama :"+nama +"\nKelas:"+kelas +"\nNPM  :"+npm);
+        // TODO code application logic here
+    }
+}
